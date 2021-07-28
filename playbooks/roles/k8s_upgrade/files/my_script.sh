125b952b77e63f61ba7057c24246c52f52722d30
Nuthan Chandra <nuthanc@juniper.net>
2021-07-28 17:22:05 +0530

    Add script

--- playbooks/roles/k8s_upgrade/files/my_script.sh
+++ playbooks/roles/k8s_upgrade/files/my_script.sh
@@ -3,3 +3,6 @@ git init
 git remote add nuthanc https://github.com/nuthanc/tf-test.git
 git fetch https://github.com/nuthanc/tf-test.git k8s_upgrade:k8s_upgrade
 docker commit nuthan_test7 nuthanc/nuthan-test-test
+# The below not required if pulling is not required
+docker login
+docker push nuthanc/nuthan-test-test
